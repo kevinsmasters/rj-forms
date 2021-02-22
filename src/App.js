ff19be1be77a4011bc9858d4c0251b71da8e8861
Kevin Masters <kevin.masters@taconic.com>
2021-02-22 17:08:47 -0500

    up to step 3

--- src/App.js
+++ src/App.js
@@ -1,23 +1,73 @@
-import logo from './logo.svg';
+import { useReducer, useState } from 'react'
 import './App.css';
 
+const formReducer = (state, event) => {
+ return {
+   ...state,
+   [event.name]: event.value
+ }
+}
+
 function App() {
+    const [formData, setFormData] = useReducer(formReducer, {});
+    const [submitting, setSubmitting] = useState(false);
+    const handleSubmit = event => {
+        event.preventDefault();
+        setSubmitting(true);
+        setTimeout(()=>{
+            setSubmitting(false);
+        }, 3000)
+    }
+
+    const handleChange = event => {
+    setFormData({
+      name: event.target.name,
+      value: event.target.value,
+    });
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="wrapper">
+        <h1>A Form</h1>
+        { submitting &&
+            <div>
+         You are submitting the following:
+         <ul>
+           {Object.entries(formData).map(([name, value]) => (
+             <li key={name}><strong>{name}</strong>: {value.toString()}</li>
+           ))}
+         </ul>
+       </div>
+        }
+        <form onSubmit={handleSubmit}>
+            <fieldset>
+                 <label>
+                   <p>Name</p>
+                   <input name="name" onChange={handleChange} />
+                 </label>
+            </fieldset>
+            <fieldset>
+         <label>
+           <p>Location</p>
+           <select name="location" onChange={handleChange}>
+               <option value="">--Please choose an option--</option>
+               <option value="us">U.S.</option>
+               <option value="de">Germany</option>
+               <option value="sw">Sweden</option>
+           </select>
+         </label>
+         <label>
+           <p>Count</p>
+           <input type="number" name="count" onChange={handleChange} step="1"/>
+         </label>
+         <label>
+           <p>Delivery</p>
+           <input type="checkbox" name="delivery" value="YEAH MON" onChange={handleChange} />
+         </label>
+       </fieldset>
+            <button type="submit">Submit</button>
+        </form>
     </div>
   );
 }
