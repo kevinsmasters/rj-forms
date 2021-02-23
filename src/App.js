ecaa6d519b82441f2108b430b6d36f3e3a5f262e
Kevin Masters <kevin.masters@taconic.com>
2021-02-23 17:04:11 -0500

    finish

--- src/App.js
+++ src/App.js
@@ -2,6 +2,14 @@ import { useReducer, useState } from 'react'
 import './App.css';
 
 const formReducer = (state, event) => {
+    if(event.reset) {
+   return {
+     location: '',
+     count: 0,
+     name: '',
+     'delivery': false,
+   }
+ }
  return {
    ...state,
    [event.name]: event.value
@@ -9,28 +17,33 @@ const formReducer = (state, event) => {
 }
 
 function App() {
-    const [formData, setFormData] = useReducer(formReducer, {});
+    const [formData, setFormData] = useReducer(formReducer, {
+        count: 100,
+    });
     const [submitting, setSubmitting] = useState(false);
     const handleSubmit = event => {
         event.preventDefault();
         setSubmitting(true);
         setTimeout(()=>{
             setSubmitting(false);
+            setFormData({
+                reset: true
+            })
         }, 3000)
     }
 
     const handleChange = event => {
-    setFormData({
-      name: event.target.name,
-      value: event.target.value,
-    });
+    // setFormData({
+    //   name: event.target.name,
+    //   value: event.target.value,
+    // });
 
     // pass status of checkbox instead of set value
-   //  const isCheckbox = event.target.type === 'checkbox';
-   // setFormData({
-   //   name: event.target.name,
-   //   value: isCheckbox ? event.target.checked : event.target.value,
-   // })
+    const isCheckbox = event.target.type === 'checkbox';
+   setFormData({
+     name: event.target.name,
+     value: isCheckbox ? event.target.checked : event.target.value,
+   })
   }
 
 
@@ -48,16 +61,16 @@ function App() {
        </div>
         }
         <form onSubmit={handleSubmit}>
-            <fieldset>
+            <fieldset disabled={submitting}>
                  <label>
                    <p>Name</p>
-                   <input name="name" onChange={handleChange} />
+                   <input name="name" onChange={handleChange} value={formData.name || ''} />
                  </label>
             </fieldset>
-            <fieldset>
+            <fieldset disabled={submitting}>
          <label>
            <p>Location</p>
-           <select name="location" onChange={handleChange}>
+           <select name="location" onChange={handleChange} value={formData.location || ''}>
                <option value="">--Please choose an option--</option>
                <option value="us">U.S.</option>
                <option value="de">Germany</option>
@@ -66,14 +79,18 @@ function App() {
          </label>
          <label>
            <p>Count</p>
-           <input type="number" name="count" onChange={handleChange} step="1"/>
+           <input type="number" name="count" onChange={handleChange} step="1"  value={formData.count || ''}/>
          </label>
          <label>
            <p>Delivery</p>
-           <input type="checkbox" name="delivery" value="YEAH MON" onChange={handleChange} />
+           <input type="checkbox"
+           name="delivery"
+           onChange={handleChange}
+           disabled={formData.location !== 'us' }
+           checked={formData['delivery'] || false} />
          </label>
        </fieldset>
-            <button type="submit">Submit</button>
+            <button type="submit" disabled={submitting}>Submit</button>
         </form>
     </div>
   );
